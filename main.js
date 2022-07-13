2bca320e7c01021e622bebaab7ce6dadfb9cec32
Ziyauddin <57740414+Ziyauddinshah@users.noreply.github.com>
2022-07-13 10:10:01 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import  { createApp }  from 'vue'
-import App from './App.vue'
-
-import JwPagination from 'jw-vue-pagination'
-const app = createApp(App)
-app.component('jw-pagination', {JwPagination});
-
-app.mount('#app')
